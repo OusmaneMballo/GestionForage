5b0d38c6614d32fcda868e437b8943dd62c8c6b4
OusmaneMballo <ousmanemballo576@gmail.com>
2020-08-31 23:03:41 +0000

    Ajout d'un user

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -6,19 +6,23 @@ use App\Entity\Role;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdministrationController extends AbstractController
 {
     private $em;
     private $user_repository;
     private $role_repository;
+    private $encoder;
 
-    public function __construct(EntityManagerInterface $emi)
+    public function __construct(EntityManagerInterface $emi, UserPasswordEncoderInterface $upei)
     {
         $this->em=$emi;
         $this->user_repository=$this->em->getRepository(User::class);
         $this->role_repository=$this->em->getRepository(Role::class);
+        $this->encoder=$upei;
     }
 
 
@@ -28,7 +32,36 @@ class AdministrationController extends AbstractController
     public function index()
     {
         $data['roles']=$this->role_repository->findAll();
+        $data['users']=$this->user_repository->findAll();
 
         return $this->render('administration/index.html.twig', $data);
+    }
+
+    /**
+     * @Route("/administrationadd", name="app_administration_add", methods={"POST|PATCH"})
+     */
+    public function add(Request $request)
+    {
+        if($request->isMethod("POST"))
+        {
+            if ($this->isCsrfTokenValid('user_token', $request->request->get('token')))
+            {
+                if($request->request->get('role')!=null)
+                {
+                    $user=new User();
+                    $user->setEmail($request->request->get("email"));
+                    $hash =$this->encoder->encodePassword($user, $request->request->get("password"));
+                    $user->setPassword($hash);
+                    $user->setPrenomNom($request->request->get("prenom_nom"));
+                    foreach ($request->request->get('role') as $role)
+                    {
+                        $user->addRole($this->role_repository->find($role));
+                    }
+                    $this->em->persist($user);
+                    $this->em->flush();
+                }
+            }
+        }
+        return $this->redirectToRoute("app_administration");
     }
 }
